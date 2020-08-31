fe7f7c315bb5106cf1eab0e00dcd0e3716668fa1
Gedeon <gedeon.astorga@gmail.com>
2020-08-31 12:45:41 -0400

    Segundo commit: Ingreso titulo y un boton, ademas se genera las referencias desde java

--- app/src/main/java/com/astorga/lista_de_tarea/MainActivity.java
+++ app/src/main/java/com/astorga/lista_de_tarea/MainActivity.java
@@ -3,13 +3,22 @@ package com.astorga.lista_de_tarea;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    // creamos la variable que hara referencia a los widgets
+    private TextView txtTitulo;
+    private Button btnIniciarAPP;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Realizar las referencias
+        txtTitulo = findViewById(R.id.txtTitulo);
+        btnIniciarAPP = findViewById(R.id.btnIniciarApp);
 
     }
 }
